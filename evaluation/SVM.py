6f30d5a990e078bab6650426b759ce358fa7f006
Quo <qhuozhihao@icloud.com>
2021-02-01 17:10:05 +0800

    新增SVC

---
+++ evaluation/SVM.py
@@ -0,0 +1,34 @@
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+from sklearn.model_selection import KFold
+from sklearn import svm
+from sklearn.metrics import f1_score
+
+def node_classify(feature, labels):
+    shape = len(labels.shape)
+    if shape == 2:
+        labels = np.argmax(labels, axis=1)
+    f1_mac = []
+    f1_mic = []
+    kf = KFold(n_splits=5, shuffle=True,random_state=0)
+    for train_index, test_index in kf.split(feature):
+        train_X, train_y = feature[train_index], labels[train_index]
+        test_X, test_y = feature[test_index], labels[test_index]
+        clf = svm.SVC(kernel='rbf')
+        clf.fit(train_X, train_y)
+        preds = clf.predict(test_X)
+
+        micro = f1_score(test_y, preds, average='micro')
+        macro = f1_score(test_y, preds, average='macro')
+        f1_mac.append(macro)
+        f1_mic.append(micro)
+    f1_mic = np.array(f1_mic)
+    f1_mac = np.array(f1_mac)
+    f1_mic = np.mean(f1_mic)
+    f1_mac = np.mean(f1_mac)
+    print('Testing based on svm: ',
+          'f1_micro=%.4f' % f1_mic,
+          'f1_macro=%.4f' % f1_mac)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.io as sio
 import time
 from models.FeatWalk import featurewalk
-from evaluation.SVM import test_classify
+from evaluation.SVM import node_classify
 '''################# Load data  #################'''
 mat_contents = sio.loadmat('data/ACM/ACM.mat')
 number_walks = 35  # 'Number of random walks to start at each instance'
@@ -20,7 +20,7 @@ Indices = np.random.randint(25, size=n)+1  # 5-fold cross-validation indices
 
 Group1 = []
 Group2 = []
-[Group1.append(x) for x in range(0, n) if Indices[x] <= 5]  # 2 for 10%, 5 for 25%, 20 for 100% of training group
+[Group1.append(x) for x in range(0, n) if Indices[x] <= 20]  # 2 for 10%, 5 for 25%, 20 for 100% of training group
 [Group2.append(x) for x in range(0, n) if Indices[x] >= 21]  # test group
 n1 = len(Group1)  # num of instances in training group
 n2 = len(Group2)  # num of instances in test group
@@ -28,9 +28,9 @@ CombX1 = X1[Group1+Group2, :]
 CombX2 = X2[Group1+Group2, :][:, Group1+Group2]
 
 start_time = time.time()
-# H_FeatWalk = featurewalk(featur1=CombX1, alpha1=.97, featur2=None, alpha2=0, Net=CombX2, beta=0, num_paths=number_walks, path_length=walk_length, dim=d, win_size=win_size).function()
-# print("time elapsed: {:.2f}s".format(time.time() - start_time))
-# sio.savemat('ACM_Embedding.mat', {"H_FeatWalk": H_FeatWalk})
+H_FeatWalk = featurewalk(featur1=CombX1, alpha1=.97, featur2=None, alpha2=0, Net=CombX2, beta=0, num_paths=number_walks, path_length=walk_length, dim=d, win_size=win_size).function()
+print("time elapsed: {:.2f}s".format(time.time() - start_time))
+sio.savemat('ACM_Embedding.mat', {"H_FeatWalk": H_FeatWalk})
 import scipy.io as io
 matr = io.loadmat('ACM_Embedding.mat')
 
@@ -43,4 +43,4 @@ labels = Label.reshape(-1)
 # class_one = np.eye(max_class)
 # labels = class_one[labels]
 
-test_classify(np.array(H_FeatWalk),labels)
+node_classify(np.array(H_FeatWalk),labels)
