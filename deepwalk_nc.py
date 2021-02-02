4b1826f00f522afe80b6dd0b27b3d33c81112c15
CELTIANqzq <9697570@qq.com>
2021-02-02 15:41:34 +0800

    参数修改

--- deepwalk_nc.py
+++ deepwalk_nc.py
@@ -5,13 +5,13 @@ from models.deepwalk import deepwalk_c
 from evaluation.node_classification import SVM
 
 
-mat_contents = sio.loadmat('data/ACM/ACM.mat')
+mat_contents = sio.loadmat('data/BlogCatalog/BlogCatalog.mat')
 start_time = time.time()
 
 deepwalk_c().deepwalkgood(rootdir='data/BlogCatalog/BlogCatalog.mat',variable_name="Network",number_walks=10,representation_size=10)
 print("time elapsed: {:.2f}s".format(time.time() - start_time))
 Label = mat_contents["Label"]
-matr2 = sio.loadmat('deepwalk_Embedding.mat')
+matr2 = sio.loadmat('Deepwalk_Embedding.mat')
 Deepwalk= matr2['Deepwalk']
 labels = Label.reshape(-1)
 SVM(np.array(Deepwalk),labels)
