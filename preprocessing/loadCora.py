9a99bf0d234a37ba87f84d06b0b5214c539ee18e
JeremyQuo <qhuozhihao@icloud.com>
2021-02-02 20:05:51 +0800

    add cora's link prediction

---
+++ preprocessing/loadCora.py
@@ -0,0 +1,162 @@
+import sys
+import pickle as pkl
+import numpy as np
+import networkx as nx
+from .normalization import fetch_normalization, row_normalize
+import scipy.sparse as sp
+import torch
+import os
+
+def create_sparse_eye_tensor(shape):
+    row = np.array(range(shape[0])).astype(np.int64)
+    col = np.array(range(shape[0])).astype(np.int64)
+    value_ = np.ones(shape[0]).astype(float)
+    indices = torch.from_numpy(np.vstack((row, col)))
+    values = torch.from_numpy(value_)
+    shape = torch.Size(shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+def save_sparse_csr(filename, array):
+    np.savez(filename, data=array.data, indices=array.indices,
+             indptr=array.indptr, shape=array.shape)
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+def load_sparse_csr(filename):
+    loader = np.load(filename)
+    return sp.csr_matrix((loader['data'], loader['indices'], loader['indptr']),
+                         shape=loader['shape'])
+
+def preprocess_citation_feat(features):
+    features = row_normalize(features)
+    return features
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+def load_citation(dataset_str="cora", normalization="AugNormAdj", use_feat=1, cuda=True):
+    """
+    Load Citation Networks Datasets.
+    """
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("data/ind.{}.{}".format(dataset_str.lower(), names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
+    test_idx_range = np.sort(test_idx_reorder)
+
+    if dataset_str == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder) + 1)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range - min(test_idx_range), :] = tx
+        tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range - min(test_idx_range), :] = ty
+        ty = ty_extended
+        features = sp.vstack((allx, tx)).tolil()
+        features[test_idx_reorder, :] = features[test_idx_range, :]
+        adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+        adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+        labels = np.vstack((ally, ty))
+        labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+        idx_test = test_idx_range.tolist()
+        idx_train = range(len(y))
+        idx_val = range(len(y), len(y) + 500)
+
+        # adj, features = preprocess_citation(adj, features, normalization)
+        features = preprocess_citation_feat(features)
+
+    elif dataset_str == 'nell.0.001':
+        # Find relation nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(allx.shape[0], len(graph))
+        isolated_node_idx = np.setdiff1d(test_idx_range_full, test_idx_reorder)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range - allx.shape[0], :] = tx
+        tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range - allx.shape[0], :] = ty
+        ty = ty_extended
+
+        features = sp.vstack((allx, tx)).tolil()
+        features[test_idx_reorder, :] = features[test_idx_range, :]
+
+        idx_all = np.setdiff1d(range(len(graph)), isolated_node_idx)
+
+        if not os.path.isfile("data/{}.features.npz".format(dataset_str)):
+            print("Creating feature vectors for relations - this might take a while...")
+            features_extended = sp.hstack((features, sp.lil_matrix((features.shape[0], len(isolated_node_idx)))),
+                                          dtype=np.int32).todense()
+            features_extended[isolated_node_idx, features.shape[1]:] = np.eye(len(isolated_node_idx))
+            features = sp.csr_matrix(features_extended)
+            print("Done!")
+            save_sparse_csr("data/{}.features".format(dataset_str), features)
+        else:
+            features = load_sparse_csr("data/{}.features.npz".format(dataset_str))
+        adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+        adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+        # adj = adj.astype(float)
+        features = features.astype(float)
+        labels = np.vstack((ally, ty))
+        labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+        idx_test = test_idx_range.tolist()
+        idx_train = range(len(y))
+        idx_val = range(len(y), len(y) + 500)
+
+        # adj, features = preprocess_citation(adj, features, normalization)
+        features = preprocess_citation_feat(features)
+
+    else:
+        features = sp.vstack((allx, tx)).tolil()
+        features[test_idx_reorder, :] = features[test_idx_range, :]
+        adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+        adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+        labels = np.vstack((ally, ty))
+        labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+        idx_test = test_idx_range.tolist()
+        idx_train = range(len(y))
+        idx_val = range(len(y), len(y) + 500)
+
+        # adj, features = preprocess_citation(adj, features, normalization)
+        features = preprocess_citation_feat(features)
+
+    # porting to pytorch
+    # features = torch.FloatTensor(np.array(features.todense())).float()
+    if use_feat:
+        features = sparse_mx_to_torch_sparse_tensor(features).float()
+    else:
+        features = create_sparse_eye_tensor(features.shape).float()
+        # features = sp.identity(features.shape[0])
+    # labels = torch.LongTensor(labels)
+    # labels = torch.max(labels, dim=1)[1]
+    # adj = sparse_mx_to_torch_sparse_tensor(adj).float()
+    # idx_train = torch.LongTensor(idx_train)
+    # idx_val = torch.LongTensor(idx_val)
+    # idx_test = torch.LongTensor(idx_test)
+    new_label=np.ones((len(labels),), dtype=int)
+    for i in range(len(labels)):
+        temp=np.where(labels[i] == 1)
+        new_label[i]=temp[0]
+
+
+    return adj, features, new_label, idx_train, idx_val, idx_test
