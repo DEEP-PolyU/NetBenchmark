ff8bbddc807a32bb8a90a09e579ad7be1cb95564
CELTIANqzq <9697570@qq.com>
2021-02-17 10:32:25 +0800

    update

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import numpy as np
 import scipy.io as sio
 import time
 from models.FeatWalk import featurewalk
-from preprocessing.utils import normalize, sigmoid, load_citation, sparse_mx_to_torch_sparse_tensor, load_citationmat
-from models.NetMF import netmf
-from evaluation.node_classification import SVM
+from models.FeatWalk_S import featurewalk_S
+from evaluation.node_classification import node_classifcation
 '''################# Load data  #################'''
 # adj, features, labels, idx_train, idx_val, idx_test = load_citation(dataset_str="cora", normalization="AugNormAdj", use_feat=1, cuda=True)
-mat_contents = sio.loadmat('data/BlogCatalog/BlogCatalog.mat')
-number_walks = 35  # 'Number of random walks to start at each instance'
+mat_contents = sio.loadmat('data/Flickr/Flickr_SDM.mat')
+number_walks = 8  # 'Number of random walks to start at each instance'
 walk_length = 25  # 'Length of the random walk started at each instance'
 win_size = 5  # 'Window size of skipgram models.'
 
@@ -29,12 +28,11 @@ n2 = len(Group2)  # num of instances in test group
 CombX1 = X1[Group1+Group2, :]
 CombX2 = X2[Group1+Group2, :][:, Group1+Group2]
 start_time = time.time()
-H_FeatWalk = featurewalk(featur1=CombX1, alpha1=.97, featur2=None, alpha2=0, Net=CombX2, beta=0, num_paths=number_walks, path_length=walk_length, dim=d, win_size=win_size).function()
+H_FeatWalk = featurewalk_S(featur1=CombX1, alpha1=.62, featur2=None, alpha2=0, Net=CombX2, beta=0, num_paths=number_walks, path_length=walk_length, dim=d, win_size=win_size).function()
 print("time elapsed: {:.2f}s".format(time.time() - start_time))
-sio.savemat('blogFeat_Embedding.mat', {"H_FeatWalk": H_FeatWalk})
+sio.savemat('Featwalk_Flickr_Embedding.mat', {"H_FeatWalk": H_FeatWalk})
 import scipy.io as io
-matr = io.loadmat('blogFeat_Embedding.mat')
-H_FeatWalk = matr['H_FeatWalk']
-labels = Label.reshape(-1)
-SVM(np.array(H_FeatWalk),labels)
+matr = io.loadmat('Featwalk_Flickr_Embedding.mat')
+H_FeatWalk_emb = matr['H_FeatWalk']
+node_classifcation(np.array(H_FeatWalk_emb),Label)
 
