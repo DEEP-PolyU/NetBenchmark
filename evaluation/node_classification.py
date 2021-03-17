effc042cea8ba7f09f7638eeef241e1bfda7de80
ricoyudog <73219750+ricoyudog@users.noreply.github.com>
2021-03-17 16:48:20 +0800

    another 10 iteration

--- evaluation/node_classification.py
+++ evaluation/node_classification.py
@@ -12,27 +12,38 @@ def node_classifcation(feature, labels):
     shape = len(labels.shape)
     if shape == 2:
         labels = np.argmax(labels, axis=1)
+    i = 1
     f1_mac = []
     f1_mic = []
-    kf = KFold(n_splits=5, shuffle=True,random_state=0)
-    for train_index, test_index in kf.split(feature):
-        train_X, train_y = feature[train_index], labels[train_index]
-        test_X, test_y = feature[test_index], labels[test_index]
-        clf = svm.SVC(kernel='rbf')
-        clf.fit(train_X, train_y)
-        preds = clf.predict(test_X)
+    for i in range(10):
+        f1_mic_fold = []
+        f1_mac_fold = []
+        kf = KFold(n_splits=5, shuffle=True,random_state=0)
+        for train_index, test_index in kf.split(feature):
+            train_X, train_y = feature[train_index], labels[train_index]
+            test_X, test_y = feature[test_index], labels[test_index]
+            clf = svm.SVC(kernel='rbf')
+            clf.fit(train_X, train_y)
+            preds = clf.predict(test_X)
 
-        micro = f1_score(test_y, preds, average='micro')
-        macro = f1_score(test_y, preds, average='macro')
-        f1_mac.append(macro)
-        f1_mic.append(micro)
-    f1_mic = np.array(f1_mic)
+            micro = f1_score(test_y, preds, average='micro')
+            macro = f1_score(test_y, preds, average='macro')
+            f1_mac_fold.append(macro)
+            f1_mic_fold.append(micro)
+
+        f1_mic_fold = np.array(f1_mic_fold)
+        f1_mac_fold = np.array(f1_mac_fold)
+        f1_mic.append(np.mean(f1_mic_fold))
+        f1_mac.append(np.mean(f1_mac_fold))
+
     f1_mac = np.array(f1_mac)
-    f1_mic = np.mean(f1_mic)
-    f1_mac = np.mean(f1_mac)
+    f1_mic = np.array(f1_mic)
+
+
+
     print('Testing based on svm: ',
-          'f1_micro=%.4f' % f1_mic,
-          'f1_macro=%.4f' % f1_mac)
+          'f1_micro=%.4f' % np.mean(f1_mic),
+          'f1_macro=%.4f' % np.mean(f1_mac))
 
 def node_classifcation_test(feature, labels):
     labels = labels.reshape(-1)
