f8a4e6f094117dd3fbf828e60dfeefb5c1f92080
CELTIANqzq <9697570@qq.com>
2021-06-26 09:21:17 +0800

    task2

--- evaluation/link_prediction.py
+++ evaluation/link_prediction.py
@@ -5,8 +5,6 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
 from preprocessing.utils import normalize, sigmoid, load_citation, sparse_mx_to_torch_sparse_tensor, load_citationANEmat
 
-
-
 def link_prediction(emb,edges_pos, edges_neg):
 
     adj_rec = np.matmul(emb, emb.T)
@@ -23,9 +21,26 @@ def link_prediction(emb,edges_pos, edges_neg):
     labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))])
     roc_score = roc_auc_score(labels_all, preds_all)
     ap_score = average_precision_score(labels_all, preds_all)
-    print("roc_score=",roc_score)
-    print("ap_score=",ap_score)
+    # print("roc_score=",roc_score)
+    # print("ap_score=",ap_score)
     return roc_score,ap_score
+
+
+def link_prediction_10_time(emb,Graph):
+    roc_score=[]
+    ap_score=[]
+    for i in range(50):
+        adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = pre.mask_val_test_edges(
+            Graph['Network'])
+        roc, ap = link_prediction(emb, edges_pos=test_edges, edges_neg=test_edges_false)
+        roc_score.append(np.array(roc))
+        ap_score.append(np.array(ap))
+    print("roc_score=",np.mean(roc_score))
+    print("ap_score=",np.mean(ap_score))
+    return np.mean(roc_score), np.mean(ap_score)
+
+
+
 
 def link_prediction_Automatic_tuning(emb, edges_pos, edges_neg):
     adj_rec = np.matmul(emb, emb.T)
