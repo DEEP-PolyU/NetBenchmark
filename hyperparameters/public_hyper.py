fe767781688fe6280a28d0edc02c91882c67dd90
JeremyQuo <JeremyQuo>
2022-05-23 00:41:43 +0800

    update end2end model

--- hyperparameters/public_hyper.py
+++ hyperparameters/public_hyper.py
@@ -4,7 +4,7 @@ SPACE_TREE = {
     'batch_size': hp.uniformint('batch_size', 1, 100),
     'nb_epochs': hp.uniformint('nb_epochs', 100, 1500),
     'lr': hp.choice('lr', [0.1, 0.01, 0.001, 0.0001, 0.005, 0.05, 0.00005]),
-    'dropout': hp.choice('dropout', [0, 0.05, 0.1, 0.2, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]),
+    'dropout': hp.choice('dropout', [0, 0.05, 0.1, 0.2, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]),
     'beta': hp.choice('beta', [0.5, 0.4, 0.3, 0.2, 0.1, 0.01, 0.001, 0.0001, 0.005, 0.05, 0.00005]),
     'alpha': hp.choice('alpha', [0.5, 0.4, 0.3, 0.2, 0.1, 0.01, 0.001, 0.0001, 0.005, 0.05, 0.00005]),
 }
